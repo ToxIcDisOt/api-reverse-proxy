583a8ba477d51b2caa14b9e8aa1d82cde799d5e0
Roastem <caplistiorimp1ng123@gmail.com>
2023-10-22 11:31:51 +0530

    .

--- app.js
+++ app.js
@@ -1,23 +1,12 @@
 const express = require('express');
 const axios = require('axios');
-const cors = require('cors');
-const rateLimit = require('express-rate-limit'); // Import the rate limiting package
-const app = express();
+const cors = require('cors');const app = express();
 const port = 3000;
 
 // Use the cors middleware to enable CORS for your app
 app.use(cors());
 
 app.use(express.json());
-
-// Apply rate limiting to the route
-const limiter = rateLimit({
-  windowMs: 100 * 60 * 1000, // 15 minutes
-  max: 3700000000, // Max 100 requests per IP in 15 minutes
-});
-
-// Apply the rate limiter to your route
-app.use('/:word', limiter);
 
 // Define a route to fetch and simplify data from the external API
 app.get('/:word', async (req, res) => {
